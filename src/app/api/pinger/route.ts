ffca501ed350d89eeb4428d4517420fbad0ee3e8
iamam305 <ajitesh314mishra@gmail.com>
2024-01-08 17:20:42 +0530

    pingu

--- src/app/api/pinger/route.ts
+++ src/app/api/pinger/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server"
 
 export const GET = async(req:NextRequest)=>{
-    NextResponse.json("suii",req)
+   return NextResponse.json("suii",req)
 }
